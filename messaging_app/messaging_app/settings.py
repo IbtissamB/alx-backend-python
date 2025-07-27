2d55b45adbc942735c824253aacc17e16d860251
IbtissamB <ibtissam.bahaj@hotmail.com>
2025-07-27 23:55:24 +0300

    initial

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -122,3 +122,24 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# REST Framework defaults (optional)
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication'    
+        'rest_framework.authentication.SessionAuthentication',         # enables login via /admin or /login
+        'rest_framework.authentication.TokenAuthentication',           # for token auth (optional)
+        'chats.auth.CustomJWTAuthentication',
+         'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend',
+        'rest_framework.filters.SearchFilter',
+        'rest_framework.filters.OrderingFilter',
+    ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 20,
+}
